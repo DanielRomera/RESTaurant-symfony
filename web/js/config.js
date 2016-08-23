c9dfe7078c55fdf54cda6265e9817c200c88e59b
Dani <daniel.romera@gmail.com>
2016-08-24 01:12:11 +0200

    Config uploaded to configurate environments for the frontend

---
+++ web/js/config.js
@@ -0,0 +1,7 @@
+/**
+ * This variable can be used to retrieve the service from different envionment, useful for testing, developing or for production
+ *  Development: app_dev.php
+ *  Production: app.php 
+ */
+
+services_url = 'app.php'
